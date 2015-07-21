8eae80aa5e58631bd5cf352776d0ad03e3a58984
Gordon Williams <gw@pur3.co.uk>
2015-07-21 14:39:10 +0100

    add shift/etc operators

--- utils.js
+++ utils.js
@@ -23,10 +23,31 @@ exports.maxType = function(a,b) {
 
 exports.getMathsOpOperator = function(op) {
   if (op.length==1) return "'"+op+"'";
-  if (op=="==") return 261; // LEX_EQUAL - see jsutils.h
-  if (op=="===") return 262; // LEX_TYPEEQUALL - see jsutils.h
-  if (op=="!=") return 263; // LEX_NEQUAL - see jsutils.h
-  if (op=="!==") return 264; // LEX_NTYPEEQUALL - see jsutils.h  
+  // see jsutils.h
+  if (op=="==") return 261; // LEX_EQUAL
+  if (op=="===") return 262; // LEX_TYPEEQUALL
+  if (op=="!=") return 263; // LEX_NEQUAL
+  if (op=="!==") return 264; // LEX_NTYPEEQUAL
+  if (op=="<=") return 265; // LEX_LEQUAL
+  if (op=="<<") return 266; // LEX_LSHIFT
+  // LEX_LSHIFTEQUAL
+  if (op==">=") return 268; //LEX_GEQUAL
+  if (op==">>") return 269; // LEX_RSHIFT
+  if (op==">>>") return 270; // LEX_RSHIFTUNSIGNED
+  // LEX_RSHIFTEQUAL
+  // LEX_RSHIFTUNSIGNEDEQUAL
+  // LEX_PLUSEQUAL
+  // LEX_MINUSEQUAL
+  // LEX_PLUSPLUS
+  // LEX_MINUSMINUS
+  // LEX_MULEQUAL
+  // LEX_DIVEQUAL
+  // LEX_MODEQUAL
+  // LEX_ANDEQUAL
+  if (op=="&&") return 281; // LEX_ANDAND
+  // LEX_OREQUAL
+  if (op=="||") return 283; // LEX_OROR
+  // LEX_XOREQUAL
   throw new Error("Unknown op '"+op+"'");
 };
 
